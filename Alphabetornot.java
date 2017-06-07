21c0b2f146ea5beb46e2a2a8627fd44700c1ccfb
dineshgs <dineshgsece99@gmail.com>
2017-06-07 14:44:52 +0530

    Add files via upload

--- Alphabetornot.java
+++ Alphabetornot.java
@@ -1,21 +1,26 @@
 package dinesh;
 import java.util.Scanner;
+
 public class Alphabetornot {
-	
-	    public static void main(String args[])
-	    {
-	        char ch;
-	        Scanner scan = new Scanner(System.in);	
-	       System.out.print("Enter a Character : ");
-	        ch = scan.next().charAt(0);
-	        
-	       if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
-	        {
-	            System.out.print(ch + " is an Alphabet");
-	        }
-	        else
-	        {
-	            System.out.print(ch + " is not an Alphabet");
-	        }
-	    }
+
+	public static void main(String[] args) {
+		char ch;
+        Scanner sc= new Scanner(System.in);
+		
+        System.out.print("Enter a Character : ");
+        ch = sc.next().charAt(0);
+		
+        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+        {
+            System.out.print(ch + " is an Alphabet");
+        }
+        else
+        {
+            System.out.print(ch + " is not an Alphabet");
+        }
+    }
+
+
 	}
+
+
