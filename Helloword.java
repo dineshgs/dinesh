c4fd76991fd66a2e9022b63f201a3a7b38d80250
dineshgs <dineshgsece99@gmail.com>
2017-06-08 14:58:01 +0530

    Add files via upload

--- Helloword.java
+++ Helloword.java
@@ -1,5 +1,9 @@
 package dinesh;
+import java.util.Scanner;
 public class Helloword { 
-	public static void main(String[] args) {
-	for(int count=0;count<5;count++)
+	public static void main(String[] args) {int a;
+	System.out.println("Enter any three numbers: ");
+	Scanner s=new Scanner(System.in);
+	a=s.nextInt();
+	for(int count=0;count<a;count++)
 	{System.out.println("hello word");}}}
