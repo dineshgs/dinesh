5f38ee7c0f10cc1874319deff04e8732b0b93b96
dineshgs <dineshgsece99@gmail.com>
2017-06-29 09:43:33 +0530

    Create  Calci.java

---
+++ Calci.java
@@ -0,0 +1,7 @@
+class Calci{  
+  void sum(int x,int y){System.out.println(x+y);}  
+  void sum(int x,int y,int z){System.out.println(x+y+z);}  
+   public static void main(String args[]){  
+  Calci obj=new Calci();  
+  obj.sum(10,10,10);  
+  obj.sum(20,20);   }}  
