abb0cd9510cd3973ee5a3f9cb57ff064e9d2376a
dineshgs <dineshgsece99@gmail.com>
2017-06-29 10:49:08 +0530

    Update Even2.java

--- Even2.java
+++ Even2.java
@@ -2,12 +2,12 @@ package example;
 import java.util.Scanner;
 public class Even2 {
 public static void main(String[] args) {
-		int i=0;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the number");
-        int a=sc.nextInt(); 
+int i=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+	int a=sc.nextInt(); 
         System.out.println("Enter the number");sc.close();
         int b=sc.nextInt();
-        for(i=a;i<=b;i++){
+	for(i=a;i<=b;i++){
         	if(i%2==0){
-        	 System.out.println("Even number: "+i);  }}}}
+System.out.println("Even number: "+i);  }}}}
